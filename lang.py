6a6f1128390a8f098c6b43ae7cb0f1993a4a81dc
yjacks <jackeyz2020@sohu.com>
2022-07-17 19:24:16 +0800

    Update lang.py

--- lang.py
+++ lang.py
@@ -6,33 +6,29 @@ checkingNetwork = ""
 didntConnectToNetwork = ""
 connectToNetwork = ""
 
-def lang():
-    import main
-
-    global exitMessage, illegalInput, checkingNetwork, didntConnectToNetwork, connectToNetwork
-    
-
+class name:
+    def __init__(self,userLanguage):
     # American English (en_US)
-    if userLanguage == "en_US":
-        # universal
-        exitMessage = "Exited Installation."
-        illegalInput = "Please input a legal option!"
+        if userLanguage == "en_US":
+            # universal
+            self.exitMessage = "Exited Installation."
+            self.illegalInput = "Please input a legal option!"
 
-        # network_check
-        checkingNetwork = "Checking Network..."
-        didntConnectToNetwork = "Seems that you didn't connect to network."
-        
-        connectToNetwork = "Do you want to connect a network? If you are SURE it's an error, enter N to skip. (Y/n)"
+            # network_check
+            self.checkingNetwork = "Checking Network..."
+            self.didntConnectToNetwork = "Seems that you didn't connect to network."
+
+            self.connectToNetwork = "Do you want to connect a network? If you are SURE it's an error, enter N to skip. (Y/n)"
 
 
-    # 简体中文 (zh_CN)
-    elif userLanguage == "zh_CN":
-        # 通用
-        exitMessage = "已退出安装程序。"
-        illegalInput = "请输入一个合法的选项！"
+        # 简体中文 (zh_CN)
+        elif userLanguage == "zh_CN":
+            # 通用
+            self.exitMessage = "已退出安装程序。"
+            self.illegalInput = "请输入一个合法的选项！"
 
-        # network_check
-        checkingNetwork = "正在检查网络..."
-        didntConnectToNetwork = "似乎您没有连接到网络。"
+            # network_check
+            self.checkingNetwork = "正在检查网络..."
+            self.didntConnectToNetwork = "似乎您没有连接到网络。"
 
-        connectToNetwork = "您要连接到网络吗？如果您*确定*这是一个错误，输入 N 以跳过。(Y/n)"
+            self.connectToNetwork = "您要连接到网络吗？如果您*确定*这是一个错误，输入 N 以跳过。(Y/n)"
